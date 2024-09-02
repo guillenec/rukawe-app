28135914459879c6d2cb1398e00a62f80bd7e275
guillenec <guillermoneculqueo@gmail.com>
2024-09-02 11:55:26 -0300

    feat: se añaden rutas y la pagina de error predeterminada

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,26 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import App from './App.jsx'
+import ErrorPage from './error-page.jsx';
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <div>Hello world!</div>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/materiales",
+    element: <div>Lista de Productos</div>,
+  },
+  {
+    path: "/usuario",
+    element: <div>Seccion Usuario</div>,
+  },
+  {
+    path: "/login",
+    element: <div>Inicio de secion / registro</div>,
   },
 ]);
 
