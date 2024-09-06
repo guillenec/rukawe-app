582b474f1919be84b43c8de8202c7e2906f1a654
guillenec <guillermoneculqueo@gmail.com>
2024-09-06 11:02:54 -0300

    feat: config tailwind

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="container  p-2 d-flex flex-column justify-content-center align-items-center overflow-hidden mt-3 gap-5">
-      <h1 className="text-red text-2xl">home</h1>
+    <div className="w-full h-screen bg-red-300 flex flex-col justify-start items-start overflow-hidden mt-2 gap-4">
+      <header className='w-full h-[80px] bg-white'>
+
+      </header>
+      <main className='w-full h-auto p-5 flex justify-center items-center'>
+        <h1 className="text-red text-2xl">home</h1>
+      </main>
     </div>
   )
 }
