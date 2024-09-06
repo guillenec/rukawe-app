0145bb37d584630c0cbb09ac185b76df3f2211ca
guillenec <guillermoneculqueo@gmail.com>
2024-09-06 11:57:24 -0300

    feat: nav menu

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Login from '../Login/Login'
+import Materiales from '../Materiales/Materiales'
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-red-300 flex flex-col justify-start items-start overflow-hidden mt-2 gap-4">
-      <header className='w-full h-[80px] bg-white'>
-
+    <div className="w-full h-screen bg-red-300 flex flex-col justify-start items-start overflow-hidden gap-4">
+      <header className='fixed w-full h-[80px] lg:w-52 lg:h-full bg-slate-800 text-white  text-base '>
+        <nav className='naviMenu w-full h-full flex flex-row justify-start items-center flex-nowrap p-2 gap-3 bg-slate-400 border-2'>
+          <a className=''>Home</a>
+          <a className=''>Login</a>
+          <a className=''>Materiales</a>
+          <a className=''>Carga Prods</a>
+        </nav>
+        <section></section>
       </header>
-      <main className='w-full h-auto p-5 flex justify-center items-center'>
+      <main className='w-full h-full lg:w-[calc(100vw-210px)] left-auto ml-auto p-5 flex justify-center items-center bg-white'>
         <h1 className="text-red text-2xl">home</h1>
       </main>
     </div>
