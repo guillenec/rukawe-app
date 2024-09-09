03cbb9635df00cc482e2462c5d905f51e5d2e1f3
guillenec <guillermoneculqueo@gmail.com>
2024-09-09 11:38:50 -0300

    feat: se cañaden variables globales para tailwind y css + fuentes para titulo y parrafo

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        titulo: ['SUSE', 'sans-serif'],
+        parrafo: ['Lato', 'sans-serif'],
+      },
+      boxShadow: {
+        custom: '2px 3px 6px #969696, -3px -3px 4px #ffffff'
+      },
+      colors: {
+        color_custom1:'var(--custom1)',
+        color_custom2:'var(--custom2)',
+        color_custom3:'var(--custom3)',
+        color_custom4:'var(--custom4)',
+        color_custom5:'var(--custom5)',
+        color_titulo:'var(--customTitulo)',
+        color_parrafo:'var(--customParrafo)',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+  ],
 }
