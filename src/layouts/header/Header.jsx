a5ceaceb617daa8b0ad9b0184069800456fc61cf
guillenec <guillermoneculqueo@gmail.com>
2024-09-16 10:31:45 -0300

    install zustand

--- src/layouts/header/Header.jsx
+++ src/layouts/header/Header.jsx
@@ -4,7 +4,7 @@ import Toggle from './components/toggle/Toggle'
 
 const Header = () => {
   return (
-    <header className=' fixed top-0 lg:left-0 w-[100%] h-[100px] lg:h-auto  text-blackCustom flex justify-center items-center font-bold text-xl uppercase text-center pt-[0] z-50'>
+    <header className=' fixed top-0 lg:left-0 w-[100%] h-[100px] lg:h-auto  text-color_custom2 flex justify-center items-center font-bold text-xl uppercase text-center pt-[0] z-50'>
       <NavBar />
       <Toggle />
     </header>
