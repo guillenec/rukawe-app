a9095b8683b3ae242a9b0ebe10e3c782a04ed7d9
guillenec <guillermoneculqueo@gmail.com>
2024-09-30 12:07:04 -0300

    feat: login pruebas

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,4 +19,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { app, db };
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+
+
+export { app, db, auth, provider };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 const Login = () => {
 
   return (
-    <div>
-      <h2>login</h2>
+    <div className='login mt-20 w-full min-h-72 border-2 rounded-md border-orange-600 flex justify-center items-center'>
+      <h1>login</h1>
+      <form action="">
+        <section className='mail p-2 flex flex-col justify-start items-start'>
+          <label htmlFor="email">Emmail</label>
+          <input id='email' type='email' name='email' />
+        </section>
+      </form>
     </div>
   )
 }
